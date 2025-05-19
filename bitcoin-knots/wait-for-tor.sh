0fac652bfb1d874f4adbb3357f402c12da965662
mysticky-philo <phil.owen@mysticky.co.uk>
2025-05-19 17:49:26 +0100

    Bug fix

--- bitcoin-knots/wait-for-tor.sh
+++ bitcoin-knots/wait-for-tor.sh
@@ -2,9 +2,18 @@
 set -e
 
 echo "Waiting for Tor to be available on tor:9050..."
-until nc -z tor 9050; do
-    sleep 1
+for i in {1..60}; do
+  if nc -z tor 9050; then
+    echo "Tor is up!"
+    break
+  fi
+  sleep 0.5
 done
 
-echo "Tor is up!"
+# Ensure the /bitcoin/.bitcoin directory is owned and writable
+echo "Fixing permissions on /bitcoin/.bitcoin..."
+mkdir -p /bitcoin/.bitcoin
+chown -R "$(id -u)":"$(id -g)" /bitcoin/.bitcoin
+chmod -R u+rwX /bitcoin/.bitcoin
+
 exec bitcoind -conf=/home/bitcoin/.bitcoin/bitcoin.conf -datadir=/bitcoin/.bitcoin
